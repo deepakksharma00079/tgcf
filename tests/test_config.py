1afa246841aaa7159842ebdd62fe811395d931ce
deepakksharma00079 <97537662+deepakksharma00079@users.noreply.github.com>
2022-01-24 10:38:54 +0530

    Update test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,17 +3,17 @@ import os
 
 logging.basicConfig(level=logging.INFO)
 
-os.environ["API_ID"] = "12345"
-os.environ["API_HASH"] = "abcd12345"
+os.environ["API_ID"] = "2303792"
+os.environ["API_HASH"] = "1e293b7a2f7a59c6b5819f7bfc29d340"
 
 os.environ[
     "TGCF_CONFIG"
 ] = """
 forwards:
 - dest:
-  - -1001656
+  - -1001620411172
   offset: '316'
-  source: -100148156
+  source: -1001751290097
 plugins: {}
 show_forwarded_from: false
 
